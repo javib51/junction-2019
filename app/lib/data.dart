10506f7471c516c75f4131264d443c1fa84f711a
kacper <kacper.skawinski@symbio.com>
2019-11-17 06:33:39 +0200

    push

--- app/lib/data.dart
+++ app/lib/data.dart
@@ -3,6 +3,13 @@ class DataStore {
   int sus_value = -1;
   int he_value = -1;
   int sav_value = -1;
+
+  bool swi_stra = false;
+  bool resell_stra = false;
+  bool invest_stra = false;
+  bool auto_stra = false;
+  bool cut_stra = false;
+
   static final DataStore instance = new DataStore._internal();
 
   factory DataStore() {
